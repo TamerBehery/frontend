6efd7a6fc982659e8360be1a98e4a5b064252500
tamerbehery <tamer_behery@hotmail.com>
2024-04-13 17:48:47 +0200

    first commit

--- clubFrontEnd/src/components/PostList.jsx
+++ clubFrontEnd/src/components/PostList.jsx
@@ -4,11 +4,11 @@ import PostCarde from "@/components/PostCarde";
 const PostList = ({ posts }) => {
   return (
     <div className="flex-col space-y-2 my-4">
-      {posts.map((post) => {
-        return <PostCarde post={post} />;
+      {posts.map((post, i) => {
+        return <PostCarde key={i} post={post} />;
       })}
-      {posts.map((post) => {
-        return <PostCarde post={post} />;
+      {posts.map((post, i) => {
+        return <PostCarde key={i} post={post} />;
       })}
     </div>
   );
