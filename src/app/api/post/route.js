40e6af871803b5eca9850bc91babba3b713c4fab
tamerbehery <tamer_behery@hotmail.com>
2024-07-22 20:45:02 +0300

    error 2

---
+++ src/app/api/post/route.js
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse} from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET(request) {
+  const posts = await prisma.post.findMany();
+  //console.log(posts);
+  return NextResponse.json({ data: posts }, { status: 200 });
+}
