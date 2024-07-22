39ce496006752370046903a492915082580cc594
tamerbehery <tamer_behery@hotmail.com>
2024-07-22 16:36:46 +0300

    error 2

--- src/app/api/post/route.js
+++ src/app/api/post/route.js
@@ -1,10 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import { NextResponse} from "next/server";
+import { NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
+//const prisma = new PrismaClient();
 
 export async function GET(request) {
-  const posts = await prisma.post.findMany();
+  //const posts = await prisma.post.findMany();
   //console.log(posts);
   return NextResponse.json({ data: posts }, { status: 200 });
 }
