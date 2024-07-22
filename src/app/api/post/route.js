3d5a0611522ee819c002be43ec1292fcc9383d00
tamerbehery <tamer_behery@hotmail.com>
2024-07-22 15:03:26 +0300

    error 2

--- src/app/api/post/route.js
+++ src/app/api/post/route.js
@@ -1,7 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse} from "next/server";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: `${process.env.DATABASE_URL}?connection_limit=40&pool_timeout=60`,
+    },
+  },
+  log: [
+    "warn",
+    "error",
+    {
+      level: "query",
+      emit: "event",
+    },
+  ],
+});
 
 export async function GET(request) {
   const posts = await prisma.post.findMany();
